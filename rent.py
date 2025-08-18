ea62180c39c41f38ae7a914cd0253b805b0cb597
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-18 20:40:01 +0530

    Total bill calculation logic added

--- rent.py
+++ rent.py
@@ -3,3 +3,5 @@ electricity_units=int(input("Enter the electricity units consumed: "))
 electricity_rate = int(input("Enter the electricity rate per unit: "))
 food=int(input("Enter the food expenses: "))
 persons=int(input("Enter the number of persons: "))
+
+total_electricity = electricity_units * electricity_rate
