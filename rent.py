764548b076fbac68e2d672b12cb0e1a70b80db49
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-18 20:36:56 +0530

    Electricity rate

--- rent.py
+++ rent.py
@@ -1,2 +1,3 @@
 rent=int(input("Enter the rent amount: "))
 electricity_units=int(input("Enter the electricity units consumed: "))
+electricity_rate = int(input("Enter the electricity rate per unit: ")) # Rate per unit of electricity
