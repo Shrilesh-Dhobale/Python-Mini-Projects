aa6c15626e9f59c1d9dcea19f127f74e2f9fd001
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-25 17:00:12 +0530

    Input task name updated

--- Task Management/task.py
+++ Task Management/task.py
@@ -4,7 +4,7 @@ def task():
 
     total_task=int(input("Enter the total number of tasks you want to complete: "))
     for i in range(0,total_task):
-        task_name=input("Enter the name of the task: ")
+        task_name=input(f"Enter the task {i+1}: ")
         task.append(task_name)
     print("Tasks added successfully!")
 
