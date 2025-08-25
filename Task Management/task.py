4d4cfeafcf65551b7176832fec2e0d7de3fc47fb
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-25 16:52:38 +0530

    Total no. of task taken as input

--- Task Management/task.py
+++ Task Management/task.py
@@ -1,3 +1,5 @@
 def task():
     task=[]
     print("=================Welcome to Task Management System=================")
+
+    total_task=int(input("Enter the total number of tasks you want to complete: "))
