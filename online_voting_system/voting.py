183554122635a32a3da0993b958f86f2975c2fa5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-05 15:33:46 +0530

    Candidate choosen by voters by taking input

--- online_voting_system/voting.py
+++ online_voting_system/voting.py
@@ -6,3 +6,4 @@ num_voters=int(input("Enter the number of voters: "))
 for i in range(num_voters):
     print(f"\nVoter {i+1}")#Iterating over voters
     print("Candidates:",", ".join(candidates))
+    vote=input("Choose your candidate: ")
