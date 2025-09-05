de3d0bfb593127bcf7a423937ad07938097a73ed
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-05 15:26:36 +0530

    Iteration performed on voters

--- online_voting_system/voting.py
+++ online_voting_system/voting.py
@@ -1,4 +1,7 @@
 candidates=["Raj","Vijay","Ajay","Ravi"]
 
-votes={candidate : 0 for candidate in candidates}
+votes={candidate : 0 for candidate in candidates}#Dict comphrehension
 num_voters=int(input("Enter the number of voters: "))
+
+for i in range(num_voters):
+    print(f"\nVoter {i+1}")#Iterating over voters
